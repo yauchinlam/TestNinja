a007663268fae8446bf2c8b5d163d711e422ee5f
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-04-24 21:59:39 -0400

    Added new unit test

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            //Arrange
+            var math = new Fundamentals.Math();
+
+            //Act
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
