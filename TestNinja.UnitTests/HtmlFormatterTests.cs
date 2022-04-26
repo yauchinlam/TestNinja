3f058a79bc0721cbdef27b199167290c37aa7ef7
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-04-25 20:46:47 -0400

    Added unit test for string return

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            //Arrange
+            var formatter = new HtmlFormatter();
+
+            //Act
+            var result = formatter.FormatAsBold("abc");
+
+            //Assert tpp specific
+            //IgnoreCase to ignore case sensistive
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            //more general
+            //Assert.That(result, Does.StartWith("<strong>"));
+
+            //A little more specific
+            //Assert.That(result, Does.StartWith("</strong>"));
+
+            //Assert.That(result, Does.Contain("abc"));
+
+            //When testing strings better to be a little more general but not too general
+
+            //Assert is case sensitive
+        }
+    }
+}
