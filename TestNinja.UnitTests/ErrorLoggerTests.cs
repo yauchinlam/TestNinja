70bb3e9291fa21c2ab45263b03b6434334835502
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-04-25 21:06:20 -0400

    Tested void method that change a property(field)

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            //Arrange
+            var logger = new ErrorLogger();
+
+            //Act
+            logger.Log("a");
+
+            //Assert
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
