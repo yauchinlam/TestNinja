d0de55099b71b9b68952e47c844abf619383a066
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-05-01 03:46:06 -0400

    Stack class test

---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class StackTests
+    {
+        [Test]
+        public void Push_ArgumentIsNull_ThrowArgNullException()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+
+            //Act and Assert
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+        
+        [Test]
+        public void Push_ValidArgs_AddTheObjectToTheStack()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+
+            //Act
+            stack.Push("a");
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+
+            //Act and Assert
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+
+            //Act and Assert
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_ReturnsObjectOnTheTop()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            var result = stack.Pop();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithAFewObjects_RemoveObjectOnTheTop()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+
+            //Act and Assert
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnObjectonTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            var result = stack.Peek();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_DoesNotRemoveTheObjectsOnTheTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            stack.Peek();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
+    }
+}
