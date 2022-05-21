5a4023d044e60b7f9a037396f24d5edca3f2b33a
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-05-21 17:46:13 -0400

    Added unit tests for employe storage

---
+++ TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteEmployeeFromDb()
+        {
+            //Arrange
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            //Act
+            controller.DeleteEmployee(1);
+
+            //Assert
+            storage.Verify(s => s.DeleteEmployee(1));
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_ReturnTypeRedirectResult()
+        {
+            //Arrange
+            var storage = new Mock<IEmployeeStorage>();
+            var controller = new EmployeeController(storage.Object);
+
+            //Act
+            var result = controller.DeleteEmployee(1);
+
+            //Assert
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+    }
+}
