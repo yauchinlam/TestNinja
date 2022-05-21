bde2d61f9900ca843804a02cfbda25eb627ab18a
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-05-21 17:30:01 -0400

    Completed InstallerHelper unit tests

---
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installHelper;
+        [SetUp]
+        public void Setup()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+
+            var result = _installHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnTrue()
+        {
+            
+            var result = _installHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
