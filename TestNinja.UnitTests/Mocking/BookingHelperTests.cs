5fc0a8081fdfd56bf66da613bd60c3d9ea5c5fe3
Yauchin M. Lam <yauchinlam17@gmail.com>
2022-12-04 18:21:57 -0500

    Added interface of IBooking Repository

---
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class BookingHelperTests
+    {
+        [Test]
+        public void OverlappingBookingExist_BookingStartsandFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
+    }
+}
