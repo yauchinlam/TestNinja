4b12fa0529fcbc7ca3ac44978b1dd17a4b825ecb
Yauchin M. Lam <yauchinlam17@gmail.com>
2023-02-26 14:32:28 -0500

    Added new test and fixed bug in test

--- TestNinja.UnitTests/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTests/Mocking/BookingHelperTests.cs
@@ -59,6 +59,19 @@ namespace TestNinja.UnitTests.Mocking
             Assert.That(result, Is.EqualTo(_existingBooking.Reference));
         }
 
+        [Test]
+        public void BookingStartsAndFinishesAfterAnExistingBooking_ReturnExistingBookingsReference()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = Before(_existingBooking.ArrivalDate),
+                DepartureDate = After(_existingBooking.DepartureDate),
+            }, _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
+
         private DateTime Before(DateTime dateTime, int days=1)
         {
             return dateTime.AddDays(-days);
